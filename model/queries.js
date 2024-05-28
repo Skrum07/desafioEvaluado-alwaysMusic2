14d6363c7883f93152eaf500a6ff6ddb4e651639
Tomas Roa <tomas.roa@gmail.com>
2024-05-28 16:24:55 -0400

    se crea carpeta model para contener las queries

---
+++ model/queries.js
@@ -0,0 +1,11 @@
+import { pool } from "../config/db.js";
+
+
+//ADD USERS
+const addUserQuery = async(user) => {
+    try {
+
+    } catch (error) {
+        console.log(`Erro code: ${error.code}, Error message: ${error.message}`);
+    }
+};
